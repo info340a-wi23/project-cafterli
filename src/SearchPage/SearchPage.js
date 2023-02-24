57bbb7714b1c0724d8cdbd1f089f54cff3ddbf97
brianc0801 <brianc81@uw.edu>
2023-02-24 03:52:27 -0800

    search page progress

---
+++ src/SearchPage/SearchPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+import { useState } from 'react';
+import { Form } from './Form.js';
+
+export function SearchPage(props){
+    return(
+        <Form />
+    )
+}
