ec1d462239d762486a8290d3fba0317c9cbd5b7c
unknown <siyul6@uw.edu>
2023-02-26 15:51:54 -0800

    mainpage nav fix

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from "./components/Footer"
 import Favicon from 'react-favicon';
+import Nav from "./components/Nav"
 
 export default function MainPage(props) {
     const [genre1, setGenre1] = useState('');
@@ -60,25 +61,7 @@ export default function MainPage(props) {
     <div className="App">
       <Favicon url="./img/music-track.png" />
     <div className="tophalf">
-      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item navbar-brand">
-            <img src="./img/music-track.png" height="50px" alt="Music Track" />
-          </li>
-          <li className="nav-item active">
-            <Link to="/" className="nav-link">Home <span className="sr-only"></span></Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/playlist" className="nav-link">Play List <span className="sr-only"></span></Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/filter" className="nav-link">Filter</Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/upload" className="nav-link">Upload <span className="sr-only"></span></Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
 
       <header className="homepage-header">
         <div className="container">
