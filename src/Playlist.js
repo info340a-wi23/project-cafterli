d8ea17508988b14540a82d984c9ed3433fbfcb0d
unknown <siyul6@uw.edu>
2023-03-12 23:29:04 -0700

    mainpage finish, fix nav on playlist

--- src/Playlist.js
+++ src/Playlist.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Nav from "./components/Nav"
 
 
 export default function Playlist(props) {
@@ -111,25 +112,7 @@ for (const key of myMap.keys()) {
 
   return (
     <div className="tophalf">  
-      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item navbar-brand">
-            <img src="./img/music-track.png" height="50px" alt="Music Track" />
-          </li>
-          <li className="nav-item active">
-            <Link to="/" className="nav-link">Home <span className="sr-only"></span></Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/playlist" className="nav-link">Play List <span className="sr-only"></span></Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/filter" className="nav-link">Filter</Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/upload" className="nav-link">Upload <span className="sr-only"></span></Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
 
       <div class="playlist_div">
         <h1>Ranking</h1> 
