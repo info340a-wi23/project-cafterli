c76326e0d348475458aaa28358a817b262179da3
cafterli <cafterli@uw.edu>
2023-03-13 19:14:52 -0700

    upload-revised

--- src/FilterPage.js
+++ src/FilterPage.js
@@ -9,16 +9,18 @@ export function FilterPage(props){
     const languageData = [...new Set(songdata.map((song) => song.language))];
     const genreData = [...new Set(songdata.map((song) => song.genre))];
 
-    const [mainFilter, setMainFilter] = useState({genre: [], language:[], artistGen:[]});
+    const [languages, setLanguages] = useState(languageData);
+    const [genres, setGenres] = useState(genreData);
 
-    const [loading, setLoading] = useState(false);
-    const [trackList, setTrackList] = useState(songdata);
+    const [mainFilter, setMainFilter] = useState({genre: [], language:[], artistGen:[]});
 
     function applyMainFilter(genre, language, artistGen, soundtrack){
         setMainFilter({genre: genre, language: language, artistGen: artistGen, soundtrack: soundtrack});
     }
 
-    const colNames = ['','Artist', 'Song', 'Genre', 'Language', 'Soundtrack','Preview'].map((name) => {
+    console.log(mainFilter);
+
+    const colNames = ['Artist', 'Song', 'Genre', 'Language', 'Soundtrack'].map((name) => {
         return <th className='searchTable' key={name}>{name.toUpperCase()}</th>
     });
 
@@ -62,44 +64,31 @@ export function FilterPage(props){
         }
     })
 
-    generateTrackList();
+    console.log(filteredGenreData);
+    console.log(filteredLanguageData);
+    console.log(filteredArtistData);
+        
 
-    
-
-    function generateTrackList(event){
-        const promises = filteredSoundTrackData.map((song) =>
-            fetch(`https://itunes.apple.com/search?term=${song.song} ${song.artist}&entity=song&limit=1`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if(data.resultCount != 0){
-                        const previewUrl = data.results[0].previewUrl;
-                        const pic = data.results[0].artworkUrl100;
-                        return { ...song, previewUrl, pic};
-                    }else{
-                        const previewUrl = null;
-                        const pic = "img/noimg.jpg";
-                        return { ...song, previewUrl, pic};
-                    }
-                })
-                .catch((error) => console.log(error))
-        );
-
-        Promise.all(promises).then((playlistWithPreviews) => {
-            setTrackList(playlistWithPreviews);
-            setLoading(false);
-          });
-    }
-
-    const songs = trackList.map((song) => {
+    const songs = filteredSoundTrackData.map((song) => {
         return <SongDataRow key={song.song} song ={song}/>
     })
 
-    const formData = [{name: "genre", data: genreData}, {name: "language", data: languageData}]
+    const formData = [{name: "genre", data: genres}, {name: "language", data: languages}]
     return(
            <><Nav/>
             <main className="flex-container-body">
-                <Form data={formData} applyMainFilter={applyMainFilter}/>
-                <Table colNames={colNames} songs={songs}/>
+                <Form data={formData} applyMainFilter={applyMainFilter}
+                />
+                <table className="searchTable">
+                    <thead>
+                    <tr>
+                        {colNames}
+                    </tr>
+                    </thead>
+                    <tbody className='searchTable'>
+                        {songs}
+                    </tbody>
+                </table>
             </main>
             <Footer/></>
     )
@@ -108,9 +97,6 @@ export function FilterPage(props){
 function SongDataRow({song}){
     return(
         <tr>
-            <td>
-                <img src={song.pic} alt={song.song}></img>
-            </td>
             <td>{song.song}</td>
             <td>{song.artist}</td>
             <td>{song.genre.charAt(0).toUpperCase() + song.genre.slice(1) }</td>
@@ -215,20 +201,5 @@ function Options(props){
                 </label>
             </div>
         </div>
-    )
-}
-
-function Table(props){
-    return(
-        <table className="searchTable">
-            <thead>
-            <tr>
-                {props.colNames}
-            </tr>
-            </thead>
-            <tbody className='searchTable'>
-                {props.songs}
-            </tbody>
-        </table>
     )
 }
